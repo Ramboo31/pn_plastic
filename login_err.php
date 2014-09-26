b0c4e40e30f3b0dbbfdbb9a7dca7550b3a29a948
graph31 <graphza.za@windowslive.com>
2014-09-27 00:27:41 +0700

    File ChkLogin

---
+++ login_err.php
@@ -0,0 +1,58 @@
+<html manifest="cache.manifest">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=1000, initial-scale=1.0, maximum-scale=1.0">
+	<title>PN Plastic : Login</title> 
+	<link rel="stylesheet" href="CSS/main.css" media="screen">
+<style type="text/css">
+
+.myButton {
+	-moz-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #ffffff;
+	box-shadow:inset 0px 1px 0px 0px #ffffff;
+	background-color:#f4d03f;
+	-moz-border-radius:6px;
+	-webkit-border-radius:6px;
+	border-radius:6px;
+	border:1px solid #dcdcdc;
+	display:inline-block;
+	cursor:pointer;
+	color:#303030;
+/*	font-family:arial;
+*/	font-size:32px;
+	font-weight:bold;
+	padding:11px 24px 2px;
+	text-decoration:none;
+	text-shadow:0px 0px 0px #ffffff;
+}
+.myButton:hover {
+	background-color:#BFBFBF;
+}
+.myButton:active {
+	position:relative;
+	top:1px;
+}
+
+
+</style>
+
+
+</head>
+<body bgcolor="FFECDB"> 
+	<font face="TH Sarabun New" >
+<center> <br><br><br><br><br><br>
+	<img src="images/icons/png/error.png"><br>
+	<span style="font-size: xx-large;">คุณยังไม่ได้ทำการเข้าสู่ระบบ</span><br>
+<!-- <table border="1">
+	<tr>
+		<td>
+		
+
+		</td>
+	</tr>
+</table> -->
+<a class="myButton" href="login.php" target="_parent" >กรุณาคลิ๊กเพื่อเข้าสู่ระบบ</a> 
+ </center>
+	</font>
+	</body>
+</head>		
